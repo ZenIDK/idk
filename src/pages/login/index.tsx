6f78ba617c5efde695ee960f1975ad2b645dc61e
Benedict Chua <benedict.chua@zendesk.com>
2023-03-02 15:53:13 +0800

    Hooked login to backend

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,24 @@
+import { supabase } from './../lib/supabaseClient';
 import { useState, useEffect, useRef } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import Layout from "@/components/layout";
 import styles from "../../styles/login.module.css";
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
  
 
-export default function login() {
+export default function login({manager, hiree}) {
   const router = useRouter();
   const [role, setRole] = useState();
   const bottomRef = useRef(null);
-  const login = async() => {
+  const [email, setEmail] = useState("");
+  const login = (e) => {
+    e.preventDefault()
+    if (manager?.some((item: { email: string; }) => item.email === email)) {
+        return router.push('/manager')
+    }
+    if (hiree?.some((item: { email: string; }) => item.email === email)) {
+        return router.push('/newbie')
+    }
     // TODO: Get role from db
     // if role == manager, router.push('/manager')
     // else, router.push('/newbie')
@@ -32,7 +41,7 @@ export default function login() {
             <p className={styles.prompt}>Tell us more about yourself</p>
             <input className={styles.input} type="text"></input>
             <p className={styles.prompt}>Email Address</p>
-            <input className={styles.email} type="text"></input>
+            <input className={styles.email} type="text" value={email} onChange={e => setEmail(e.target.value)}></input>
             <br />
             <button className={styles.button} onClick={login}>LOGIN</button>
           </form>
@@ -41,3 +50,15 @@ export default function login() {
     </>
   )
 }
+
+export async function getServerSideProps() {
+    let { data: managerdata } = await supabase.from('Manager').select()
+    let { data:hireedata } = await supabase.from('Hiree').select()
+    return {
+      props: {
+       manager: managerdata,
+       hiree: hireedata
+      },
+    }
+  }
+  
