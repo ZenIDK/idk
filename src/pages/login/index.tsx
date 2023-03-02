7f1b2f22ae02eb37edce24ce253785d9ca763ed9
Louis-Zendesk <louis@zendesk.com>
2023-03-02 09:58:00 +0800

    complete login page frontend

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,17 +1,32 @@
-import Layout from "@/components/layout"
-import Welcome from "@/components/welcome"
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Layout from "@/components/layout";
+import styles from "../../styles/login.module.css";
 
-function login() {
+
+export default function login() {
+  const router = useRouter();
+  const [role, setRole] = useState();
+  const login = async() => {
+    // TODO: Get role from db
+    // if role == manager, router.push('/manager')
+    // else, router.push('/newbie')
+  }
+
   return (
     <>
-      <Welcome />
+      
       <Layout>
-        <div className="login">
-          {/* TODO: Text box, login form, submit button, button routing */}
+        <div className={styles.login}>
+          <p className={styles.header}>Welcome to Zendesk</p>
+          <p className={styles.prompt}>Tell us more about yourself</p>
+          <input className={styles.input} type="text"></input>
+          <p className={styles.prompt}>Email Address</p>
+          <input className={styles.email} type="text"></input>
+          <br />
+          <button className={styles.button} onClick={login}>LOGIN</button>
         </div>
       </Layout>
     </>
   )
 }
-
-export default login
