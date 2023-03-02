e85859d717825fc11381da37066c19738c1c0c34
Louis-Zendesk <louis@zendesk.com>
2023-03-02 11:43:34 +0800

    complete navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,19 @@
+import React from 'react'
+import styles from '../styles/navbar.module.css'
+import Link from 'next/link'
+
+ 
 export default function Navbar() {
     return (
-        <>
-            <div className="navbar">
-                
+        <nav className={styles.mainnav}>
+            <div className={styles.team}>
+                <Link href='/'>iDK.</Link>
             </div>
-        </>
+            <div className={styles.list}>
+                <Link className={styles.item} href='/newbie'>Tasks</Link>
+                <Link className={styles.item} href='/glossary'>Glossary</Link>
+                <Link className={styles.item} href='/channel'>Channels</Link>
+            </div>
+        </nav>
     )
 }
