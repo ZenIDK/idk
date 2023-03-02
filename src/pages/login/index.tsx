a21abb88d72c774d101c42124784575335fa72f9
Louis-Zendesk <louis@zendesk.com>
2023-03-02 15:48:15 +0800

    prepare login for data query to backend

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -9,7 +9,28 @@ export default function login() {
   const router = useRouter();
   const [role, setRole] = useState();
   const bottomRef = useRef(null);
-  const login = async() => {
+  const login = async(event) => {
+    event.preventDefault()
+    
+    const data = {
+      email: event.target.email.value,
+    }
+
+    const jsonData = JSON.stringify(data)
+    //DB endpoint, if needed
+    const endpoint = ''
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonData
+    }
+
+    const response = await fetch(endpoint, options)
+
+    //use response to determine role
+
     // TODO: Get role from db
     // if role == manager, router.push('/manager')
     // else, router.push('/newbie')
@@ -32,7 +53,7 @@ export default function login() {
             <p className={styles.prompt}>Tell us more about yourself</p>
             <input className={styles.input} type="text"></input>
             <p className={styles.prompt}>Email Address</p>
-            <input className={styles.email} type="text"></input>
+            <input className={styles.email} type="text" id="email" name="email" required></input>
             <br />
             <button className={styles.button} onClick={login}>LOGIN</button>
           </form>
