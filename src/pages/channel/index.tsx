3df1571f842ea5f690e764745e170699e80917f1
Jonathan Kong <jonathan.kong@zendesk.com>
2023-03-03 12:34:58 +0800

    LAST HURRAH

--- src/pages/channel/index.tsx
+++ src/pages/channel/index.tsx
@@ -1,58 +1,87 @@
-import { useState } from "react";
-import Navbar from "@/components/navbar";
-import styles from "../../styles/channel.module.css";
+import { useState } from 'react'
+import Navbar from '@/components/navbar'
+import styles from '../../styles/channel.module.css'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import Everythingelse from '@/components/everythingelse'
 
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Channel() {
-    const [channel, setChannel] = useState();
-    const mockData = {};
-    const submit = async() => {
-        //TODO: Get keyword from db, setChannel if exist in mockData
-    }
+  const [channel, setChannel] = useState()
+  const [isShown, setIsShown] = useState(false)
+  const mockData = {}
 
-    const Results = () => {
-        return (
-            <p>You can try the {channel} channel</p>
-        );   
-    }
+  const handleClick = (event) => {
+    event.preventDefault()
+    // 👇️ toggle shown state
+    setChannel(event.value)
+    setIsShown((current) => !current)
 
+    // 👇️ or simply set it to true
+    // setIsShown(true);
+  }
+
+  const Results = () => {
     return (
-        <>
-            <div className={styles.channel}>
-                <p className={styles.title}>What are you looking for?</p>
-                <form>
-                    <input type="text" className={styles.input}/>
-                    <br />
-                    <button className={styles.button} onClick={submit}>Search</button>
-                </form>
-                <div className={styles.results}>
-                    {channel ? <Results />: <p>Unfortunately, we do not have a channel for that</p>}
-                </div>
-
-                <div className={styles.taskContainer2}>
-                    <h2 className={inter.className}>&& Everything else 🔮</h2>
-                    <br></br>
-                    <div className={inter.className}>
-                        <div className={styles.parentContainer2}>
-                            <Link href={`/glossary`}>
-                                <Everythingelse name={'Glossary'} icon={'/glossary.png'}/>
-                            </Link>
-                            <Link href={`/channel`}>
-                                <Everythingelse name={'Channels'} icon={'/slack-logo.png'} />
-                            </Link>
-                            <Link href={`/newbie`}>
-                                <Everythingelse name={'Your Team'} icon={'/teamwork.png'} />
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
+      <p>
+        You can try the <span style={{ color: 'purple' }}>#ask-{channel}</span>{' '}
+        channel, on Slack. 🙌
+      </p>
     )
-}
+  }
 
+  //   const handleSubmit = (event) => {
+  //     event.preventDefault()
+  //     if (channel === 'falcon') {
+  //     }
+  //   }
+
+  return (
+    <>
+      <div className={inter.className}>
+        <div className={styles.channel}>
+          <p className={styles.title}>What are you looking for?</p>
+          <form>
+            <input
+              type='text'
+              className={styles.input}
+              onChange={(event) => setChannel(event.target.value)}
+            />
+            <br />
+            {/* <button className={styles.button} onClick={handleClick}>
+              Search
+            </button> */}
+          </form>
+
+          {/* {isShown && <Results />} */}
+          <div className={styles.results}>
+            {channel ? (
+              <Results />
+            ) : (
+              <p>Unfortunately, we do not have a channel for that! 😪</p>
+            )}
+          </div>
+
+          <div className={styles.taskContainer2}>
+            <h2 className={inter.className}>&& Everything else 🔮</h2>
+            <br></br>
+            <div className={inter.className}>
+              <div className={styles.parentContainer2}>
+                <Link href={`/glossary`}>
+                  <Everythingelse name={'Glossary'} icon={'/glossary.png'} />
+                </Link>
+                <Link href={`/channel`}>
+                  <Everythingelse name={'Channels'} icon={'/slack-logo.png'} />
+                </Link>
+                <Link href={`/newbie`}>
+                  <Everythingelse name={'Your Team'} icon={'/teamwork.png'} />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/pages/glossary/index.tsx
+++ src/pages/glossary/index.tsx
@@ -1,55 +1,77 @@
-import Navbar from "@/components/navbar";
-import styles from "../../styles/glossary.module.css";
+import Navbar from '@/components/navbar'
+import styles from '../../styles/glossary.module.css'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import Everythingelse from '@/components/everythingelse'
 
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Glossary() {
-    const mockData = {"ZDI": "Zendesk Development Interface", "Zopim": "The company Zendesk acquired and rebranded to Zendesk Chat", "a":"A", "b":"B"}
+  const mockData = {
+    ZDI: 'Zendesk Development Interface',
+    Zopim: 'The company Zendesk acquired and rebranded to Zendesk Chat',
+    ARPU: 'Average Revenue Per User (agent)',
+    ARR: 'Annual Recurring Revenue',
+    ASAT: 'Advocate Satisfaction',
+    ASP: 'Average Selling Price',
+    AWS: 'Amazon Web Services',
+    BPO: 'Business Process Outsourcing',
+    CIL: 'Comments In-Line',
+    COMET: 'Calendar of Many Exciting Things (internal calendar)',
+    CPL: 'Cost Per Lead',
+    DB: 'Database',
+    EDW: 'Annual Recurring Revenue',
+    EOL: 'End of life',
+    ESPP: 'Employee Stock Purchase Program',
+    GA: 'General Availability (also Google Analytics)',
+    MRR: 'Monthly Recurring Revenue (in machine learning this is : Mean Reciprocal Rank)',
+    PaaS: 'Platform-as-a-Service',
+    QBR: 'Quarterly Business Review',
+    REST: '(API) Representational State Transfer'
+  }
 
-    return (
-        <>
-            <div className={styles.glossary}>
-                <h1 className={styles.title}>
-                    Looking for Terminologies?!
-                </h1>
-                <div className={styles.outer}>
-                    <div className={styles.container}>
-                        <div className={styles.header}>
-                            <p>Term</p>
-                            <p>Definition</p>
-                        </div>
-                        {Object.keys(mockData).map((key, index) => {
-                            return (
-                                <div className={styles.pair} key={index}>
-                                    <p className={styles.item}>{key}</p>
-                                    <p className={styles.definition}>{mockData[key]}</p>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>
-                <div className={styles.taskContainer2}>
-                    <h2 className={inter.className}>&& Everything else 🔮</h2>
-                    <br></br>
-                    <div className={inter.className}>
-                        <div className={styles.parentContainer2}>
-                            <Link href={`/glossary`}>
-                                <Everythingelse name={'Glossary'} icon={'/glossary.png'}/>
-                            </Link>
-                            <Link href={`/channel`}>
-                                <Everythingelse name={'Channels'} icon={'/slack-logo.png'} />
-                            </Link>
-                            <Link href={`/newbie`}>
-                                <Everythingelse name={'Your Team'} icon={'/teamwork.png'} />
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className={inter.className}>
+        <div className={styles.glossary}>
+          <h1 style={{ marginBottom: '50px', marginTop: '30px' }}>
+            📖 Looking for Terminologies?!
+          </h1>
+          <div className={styles.outer}>
+            <div className={styles.container}>
+              <div className={styles.header}>
+                <p style={{ color: 'green' }}>Term</p>
+                <p style={{ color: 'green' }}>Definition</p>
+              </div>
+              {Object.keys(mockData).map((key, index) => {
+                return (
+                  <div className={styles.pair} key={index}>
+                    <p className={styles.item}>{key}</p>
+                    <p className={styles.definition}>{mockData[key]}</p>
+                  </div>
+                )
+              })}
             </div>
-        </>
-    )
+          </div>
+          <div className={styles.taskContainer2}>
+            <h2 className={inter.className}>&& Everything else 🔮</h2>
+            <br></br>
+            <div className={inter.className}>
+              <div className={styles.parentContainer2}>
+                <Link href={`/glossary`}>
+                  <Everythingelse name={'Glossary'} icon={'/glossary.png'} />
+                </Link>
+                <Link href={`/channel`}>
+                  <Everythingelse name={'Channels'} icon={'/slack-logo.png'} />
+                </Link>
+                <Link href={`/newbie`}>
+                  <Everythingelse name={'Your Team'} icon={'/teamwork.png'} />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
