4ceb7b255a475e725cb63a3abfb23ca1c0bf32f4
Louis-Zendesk <louis@zendesk.com>
2023-03-03 12:09:34 +0800

    set up manager's task data placeholder

--- src/pages/manager/task/[pid].tsx
+++ src/pages/manager/task/[pid].tsx
@@ -8,6 +8,9 @@ const Task = () => {
   const router = useRouter()
   const { pid } = router.query
   const [tasksdata, setTaskdata] = useState()
+  const [detail, setDetail] = useState()
+  const [steps, setSteps] = useState()
+  const [video, setVideo] = useState()
   const complete = async() => {
     //TODO: update tasksdata with the data from taskboard
     const { error } = await supabase.from('Tasks').insert(tasksdata)
@@ -20,13 +23,13 @@ const Task = () => {
         <div className={styles.task}>
             <p className={styles.welcome}>Welcome to Task {pid}</p>
             <p className={styles.header}>Details</p>
-            <input className={styles.content}/>
+            <input className={styles.content}>{detail}</input>
             <p className={styles.header}>Steps</p>
-            <input className={styles.content}/>
+            <input className={styles.content}>{steps}</input>
             <p className={styles.header}>Video URL</p>
-            <input className={styles.content}/>
+            <input className={styles.content}>{video}</input>
             <p className={styles.header}>Takes Approximately</p>
-            <input className={styles.content}/>
+            <input className={styles.content}>60 minutes</input>
             <button className={styles.button} onClick={complete}>Complete</button>
         </div>
     </>
